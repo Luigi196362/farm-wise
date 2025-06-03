c2e09001fa4b56a0ad28a9bd4f3c971dde5e2a2a
Luis Enrique Romero Perez <wichobros@gmail.com>
2025-06-03 11:05:24 -0600

    env

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,13 @@ const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
+    process.env.DB_PORT,
     {
         host: process.env.DB_HOST,
+        name: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
         dialect: 'postgres',
         logging: false,
     }
